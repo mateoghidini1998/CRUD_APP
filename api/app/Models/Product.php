f4d4bc1861571c3fa64d3ba9bb5870d62e2782c1
mateoghidini1998 <mateghidini123@gmail.com>
2022-05-16 17:33:32 -0300

    search bar working and new styles

---
+++ api/app/Models/Product.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+    protected $table = 'products';
+    protected $fillable = ['description', 'price', 'stock'];
+    public $timestamps = false;
+}
